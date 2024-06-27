00d85c567674515de6f4f5dc27444d09ca1b0dc6
Yagnik007 <yagniktalaviya4122002@gmail.com>
2024-06-27 17:42:37 +0530

    Handling URLs

--- 19urls/main.go
+++ 19urls/main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=jh324b5kj34323"
 
 func main() {
 	fmt.Println("URLS")
-}
+	fmt.Println(myurl)
 
+	//parsing
+	result, _ := url.Parse(myurl)
+	fmt.Println("Scheme:", result.Scheme)
+	fmt.Println("Host:", result.Host)
+	fmt.Println("Port:", result.Port())
+	fmt.Println("Path:", result.Path)
+	fmt.Println("RawQuery:", result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("Type of qparams is: %T\n", qparams) //Output: url.Values
+	fmt.Println(qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println(val)
+	}
+
+	//We have to pass reference ('&') always and for sure and nothing needs to change in below syntax
+	partsOfUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "lco.dev",
+		Path:     "/tutcss",
+		RawQuery: "user=hitesh",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
+}
